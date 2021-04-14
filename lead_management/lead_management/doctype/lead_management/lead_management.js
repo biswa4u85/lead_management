e1f6b95f3e3d2336675e13791e1cda78d539c29c
biswa4u85 <biswa4u85@gmail.com>
2021-04-14 11:06:25 +0000

    upd files

---
+++ lead_management/lead_management/doctype/lead_management/lead_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, info@erptech.in and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Lead Management', {
+	// refresh: function(frm) {
+
+	// }
+});
